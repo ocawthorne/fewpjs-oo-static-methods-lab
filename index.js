5d37c01c3de18f0769683f4a54b637ee21b95783
ocawthorne <ocawthorne@gmail.com>
2021-01-24 17:32:08 +0000

    commit tree 33c4c71121e1516f3aa9672e38de7f2f1009cb2e

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^-,\w\s'A-Za-z0-9]+/g, '');
+  }
+
+  static titleize(str) {
+    const EXCLUDED = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let i = 0
+    return str.split(' ').map(element => {
+      i++
+      if (EXCLUDED.indexOf(element) > -1 && i !== 1) return this.sanitize(element)
+      return this.capitalize(element)
+    }).join(' ')
+  }
 }
+
+// FIRST_WORD   EXCLUDED_WORD   ACTION?
+//     NO            NO           YES 
+//     NO            YES          NO    (Only ignore if NOT first word BUT excluded word (line 16))
+//     YES           NO           YES 
+//     YES           YES          YES 
